0b43928c9551eeda74d20177b76c3e48645f4d84
Mebin Jacob <mebinjacob@gmail.com>
2015-04-01 15:20:37 -0400

    after login page

--- demo1/src/main/java/dbms/controller/MyController.java
+++ demo1/src/main/java/dbms/controller/MyController.java
@@ -9,15 +9,23 @@ import dbms.service.AdminService;
 
 @Controller
 public class MyController {
-	
+
 	@Autowired 
 	AdminService ser;	
-	 @RequestMapping("/login")
-	    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-	      model.addAttribute("name", name);  
-//		  ser.test();
-	      return "login";
-		  
-	    }
+
+	@RequestMapping("/login")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);  
+		//		  ser.test();
+		return "login";
+	}
+	
+	@RequestMapping("/loggedin")
+	public String loggedIn(@RequestParam(value="username") String username, @RequestParam(value="password")String password){
+		System.out.println("The username is "  );
+		System.out.println("The password is " );
+		return "index";
+	}
+
 
 }

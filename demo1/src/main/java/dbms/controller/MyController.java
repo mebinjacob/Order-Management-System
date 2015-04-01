8e8079ace07dc4436e2f6bfd084e5d3e777e30eb
Mebin Jacob <mebinjacob@gmail.com>
2015-04-01 14:44:46 -0400

    css and style

--- demo1/src/main/java/dbms/controller/MyController.java
+++ demo1/src/main/java/dbms/controller/MyController.java
@@ -1,8 +1,5 @@
 package dbms.controller;
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +15,7 @@ public class MyController {
 	 @RequestMapping("/login")
 	    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
 	      model.addAttribute("name", name);  
-		  ser.test();
+//		  ser.test();
 	      return "login";
 		  
 	    }

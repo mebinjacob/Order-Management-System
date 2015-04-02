2d1bee39824d02e9cdd4e317642119754d1ff811
Mebin Jacob <mebinjacob@gmail.com>
2015-04-02 16:18:14 -0400

    google graph integrated!!!

--- demo1/src/main/java/dbms/controller/MyController.java
+++ demo1/src/main/java/dbms/controller/MyController.java
@@ -1,10 +1,17 @@
 package dbms.controller;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import dbms.dao.Test;
 import dbms.service.AdminService;
 import dbms.service.LoginService;
 
@@ -33,5 +40,51 @@ public class MyController {
 		return "login";
 	}
 
+	@RequestMapping("/displayGraph")
+	public String displayGraph(){
 
+		return "graph";
+	}
+	
+	//for strint and int
+	@RequestMapping("/getData")
+	public @ResponseBody List<List<Object>> getSampleData(){
+		Test t = new Test();
+		t.setPoint(1);
+		List<Test> tList = new ArrayList<Test>();
+		Test t1 = new Test();
+		t.setName("Mushroom");
+		t.setPoint(1);
+		tList.add(t1);
+		
+		List<List<Object>> arrayList = new ArrayList<List<Object>>(); 
+		List<Object> mushroom = new ArrayList<Object>();
+		mushroom.add("Mushroom");
+		mushroom.add(1);
+		
+		List<Object> onions = new ArrayList<Object>();
+		onions.add("onions");
+		onions.add(3);
+		
+		List<Object> olives = new ArrayList<Object>();
+		olives.add("Olives");
+		olives.add(4);
+		
+		List<Object> Zucchini = new ArrayList<Object>();
+		Zucchini.add("Zucchini");
+		Zucchini.add(4);
+		
+		
+		List<Object> Pepperoni = new ArrayList<Object>();
+		Pepperoni.add("Pepperoni");
+		Pepperoni.add(4);
+		
+		arrayList.add(mushroom);
+		arrayList.add(onions);
+		arrayList.add(olives);
+		arrayList.add(Zucchini);
+		arrayList.add(Pepperoni);
+		
+		return arrayList;//"[ ['Mushrooms', 3], ['Onions', 1], ['Olives', 1], ['Zucchini', 1], ['Pepperoni', 2]]";
+	}
 }
---
+++ demo1/src/main/java/dbms/dao/Test.java
@@ -0,0 +1,24 @@
+package dbms.dao;
+
+public class Test {
+	
+	
+	private int point;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	
+
+}

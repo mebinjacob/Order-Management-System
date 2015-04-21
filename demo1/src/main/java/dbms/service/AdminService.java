364a5fcd48432ea718b9806fc246c6e7aff8352f
piyush <piyush.johar@yahoo.co.in>
2015-04-21 07:07:13 -0400

    dsd

--- demo1/src/main/java/dbms/service/AdminService.java
+++ demo1/src/main/java/dbms/service/AdminService.java
@@ -24,7 +24,7 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.jdbc.support.rowset.SqlRowSetMetaData;
 import org.springframework.stereotype.Service;
 
-import scala.Array;
+//import scala.Array;
 import dbms.dao.Item;
 
 @Service
